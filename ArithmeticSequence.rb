b835822666cc8b2ed3c8b67068eca8d6fe17f565
pfox27 <80210760+pfox27@users.noreply.github.com>
2026-03-06 08:37:42 -0500

    update

---
+++ ArithmeticSequence.rb
@@ -0,0 +1,45 @@
+#  ArithmeticSequence.rb
+#  
+#  Copyright 2026 Paul Fox <paulfox@Paul-Fox-Air.local>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+class StdClass
+
+  def initialize
+    puts "Enter the common difference for the arithmetic sequence: "
+    d = gets.to_i
+    termN = 1
+    puts "Enter the first term for the arithmetic sequence: "
+    a = gets.to_i
+    puts "Enter the number of terms you'd like listed for the arithmetic sequence: "
+    maxIndex = gets.to_i
+    n = 1
+		
+while n <= maxIndex 
+  termN = a + (n - 1)*d
+  puts "Term #{n} is #{termN}" 
+  n+=1
+end
+
+  end
+end
+x = StdClass.new
+
+
